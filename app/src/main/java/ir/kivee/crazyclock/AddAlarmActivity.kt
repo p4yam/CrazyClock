0df99026fda4486bf8c4ac9ad6cdf285a627d46b
p4yaam <p4yaam@gmail.com>
2018-11-10 16:39:58 +0330

    changing ui

--- app/src/main/java/ir/kivee/crazyclock/AddAlarmActivity.kt
+++ app/src/main/java/ir/kivee/crazyclock/AddAlarmActivity.kt
@@ -1,18 +1,48 @@
 package ir.kivee.crazyclock
 
+import android.app.Activity
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_add_alarm.*
 import java.util.*
 
+
 class AddAlarmActivity : AppCompatActivity() {
 
+    val RINGTONE_REQUEST_CODE = 5
+    var currentUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_alarm)
         handleWheel()
         getNow()
         enableVibration()
+        add_alarm_ringtone.setOnClickListener {
+            val intent = Intent(RingtoneManager.ACTION_RINGTONE_PICKER)
+            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALARM)
+            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Ringtone")
+            intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, currentUri)
+            this.startActivityForResult(intent, RINGTONE_REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RINGTONE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val uri = data?.getParcelableExtra<Uri>(RingtoneManager.EXTRA_RINGTONE_PICKED_URI)
+            if (uri != null) {
+                Log.d("p4yam", "Uri is: $uri")
+                currentUri = uri
+                val ringtone = RingtoneManager.getRingtone(this, uri)
+                add_alarm_ringtone_name.text = ringtone.getTitle(this)
+            }
+
+        }
+
     }
 
     private fun handleWheel() {
